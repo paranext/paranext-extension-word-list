9e4114ff7b18c440c6a39c5dfddd76b450e68616
Rolf Heij <rolf_heij@sil.org>
2023-10-30 15:43:07 +0100

    Add open word list command

--- src/types/paranext-extension-word-list.d.ts
+++ src/types/paranext-extension-word-list.d.ts
@@ -2,3 +2,15 @@ declare module 'paranext-extension-word-list' {
   // Add extension types exposed on the papi for other extensions to use here
   // More instructions can be found in the README
 }
+
+declare module 'papi-shared-types' {
+  export interface CommandHandlers {
+    /**
+     * Opens a new word list WebView and returns the WebView id
+     * @param projectId Project ID to open with the word list. Prompts the user to
+     * select project if not provided
+     * @returns WebView id for new word list WebView or `null` if the user canceled the dialog
+     */
+    'paratextWordList.open': (projectId?: string) => Promise<string | null | undefined>;
+  }
+}
